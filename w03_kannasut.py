af76f9d878c774860ffe3b2c980da10d92a2682d
26_kannasut <el2326@signalschool.ac.th>
2026-06-24 11:35:28 +0700

    Add files via upload

---
+++ w03_kannasut.py
@@ -0,0 +1,14 @@
+# if -else
+a = int(input())
+if a >= 1:
+    print("ค่าเป็นบวก")
+elif a == 0:
+    print("ค่าเป็นศูยน์")
+else:
+    print("ค่าเป็นลบ")
+
+# Username Password
+username = "kannasut"
+password = 1234
+
+if username == "kannasut" and password
