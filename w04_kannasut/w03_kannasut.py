3fea89bb12e2f97edec7a300a9fa7a6bcc82053e
26_kannasut <el2326@signalschool.ac.th>
2026-06-24 12:13:22 +0700

    ส่งงาน 2

---
+++ w04_kannasut/w03_kannasut.py
@@ -0,0 +1,17 @@
+# if -else
+#a = int(input())
+#if a >= 1:
+    #print("ค่าเป็นบวก")
+#elif a == 0:
+    #print("ค่าเป็นศูยน์")
+#else:
+   # print("ค่าเป็นลบ")
+
+# Username Password
+username = input("username")
+password = int(input("password"))
+
+if username == "kannasut" and password == 1234:
+    print("เข้าสู่ระบบสำเร็จ")
+else:
+    print("เข้าสู่ระบบไม่สำเร็จ")
